ec7c9dd04ec62479cb1cee95805a339a40bee003
Justin Reynolds <Jreynolds97@comcast.net>
2019-05-05 14:51:55 -0600

    Added sed to allow file paths to have spaces in them

--- discord_cache.sh
+++ discord_cache.sh
@@ -2,5 +2,6 @@
 b=$(pwd)
 echo "Finding Discord Cache Directory"
 a=$(find / -name [cC]ache 2>/dev/null | grep [dD]iscord)
+a=$(echo $a | sed -e 's/ /\\ /g')
 file $a/* > $b/dump_results.txt
 echo "Discord Cache directory: $a"
