728b5e06c0c56ddd2147e875cc7096fd30322520
Justin Reynolds <Jreynolds97@comcast.net>
2019-06-08 00:16:34 -0600

    Finally resolved broken paths with spaces

--- discord_cache.sh
+++ discord_cache.sh
@@ -2,6 +2,21 @@
 b=$(pwd)
 echo "Finding Discord Cache Directory"
 a=$(find / -name [cC]ache 2>/dev/null | grep [dD]iscord)
-a=$(echo $a | sed -e 's/ /\\ /g')
-file $a/* > $b/dump_results.txt
-echo "Discord Cache directory: $a"
+#a=$(echo $a | sed -e 's/ /\\ /g')
+c=''
+for i in $a
+do
+	if [ -z "$c" ] 
+	then
+		c="$i"
+	else
+		c="$c\\$i"
+	fi
+done
+if [ -d "$c" ]
+then
+	file $c/* > $b/dump_results.txt
+	echo "Discord Cache directory: $c"
+else
+	echo "Could not find cache directory"
+fi
